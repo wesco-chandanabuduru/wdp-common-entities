cd6a3d5e06a5baa9a4eea0c5fc1a0b1887061f4d
wesco-chandanabuduru <102545118+wesco-chandanabuduru@users.noreply.github.com>
2022-07-07 14:37:26 +0530

    changed note class

--- src/main/java/com/wesco/model/Note.java
+++ src/main/java/com/wesco/model/Note.java
@@ -1,4 +1,4 @@
-package com.wesco.notes.model;
+package com.wesco.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.CreatedDate;
@@ -9,9 +9,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
-/**
- * Created by rajeevkumarsingh on 27/06/17.
- */
 @Entity
 @Table(name = "notes")
 @EntityListeners(AuditingEntityListener.class)
